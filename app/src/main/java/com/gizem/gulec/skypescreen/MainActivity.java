4d02d3da09fa318c7cd93e2646e64a3f193da99b
gizemgulec <gizem.gulec@bil.omu.edu.tr>
2021-01-27 12:46:11 +0300

    Skype Screen

---
+++ app/src/main/java/com/gizem/gulec/skypescreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gizem.gulec.skypescreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
